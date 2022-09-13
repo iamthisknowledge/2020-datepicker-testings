0ae2a330b5ed488680a013df0bc022c1e49f3ccf
rodrigo <rodrigo@pop-os.localdomain>
2022-09-13 17:00:00 +0200

    2020 commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useReducer} from 'react'
+import {DateRangeInput} from '@datepicker-react/styled'
+
+const initialState = {
+  startDate: null,
+  endDate: null,
+  focusedInput: null,  
+}
+
+
+
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'focusChange':
+      return {...state, focusedInput: action.payload}
+    case 'dateChange':
+      return action.payload
+    default:
+      throw new Error()
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <DateRangeInput
+      onDatesChange={data => dispatch({type: 'dateChange', payload: data})}
+      onFocusChange={focusedInput => dispatch({type: 'focusChange', payload: focusedInput})}
+      startDate={state.startDate} // Date or null
+      endDate={state.endDate} // Date or null
+      focusedInput={state.focusedInput} // START_DATE, END_DATE or null
+    />
+  )
 }
 
 export default App;
